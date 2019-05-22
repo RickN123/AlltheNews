3f8afae54f5aa30f61f23f8ec6db65b121ca2ba8
RickN123 <nicolai.richard@gmail.com>
2019-05-22 14:27:42 -0400

    updated H

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdb";
+var MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
