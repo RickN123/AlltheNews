d269b9de3935fdca06ad968cb690f8a71639646f
RickN123 <nicolai.richard@gmail.com>
2019-05-21 22:11:07 -0400

    summary

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
 var PORT = 3000;
 
 var app = express();
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -30,6 +32,9 @@ app.get("/scrape", function (req, res) {
         .children("a")
         .text();
       result.link = $(this)
+        .children("a")
+        .attr("href");
+      result.summary = $(this)
         .children("a")
         .attr("href");
 
