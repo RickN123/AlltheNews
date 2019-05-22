d5c21d61e6b7ae233cb3441ba19c0e4073fdf923
RickN123 <nicolai.richard@gmail.com>
2019-05-22 10:08:15 -0400

    heroku change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdb";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
